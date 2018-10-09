1e77a4374abf3aaf609b013ed27bb2055d449e60
Raphael Marques <raphaelmarques77@gmail.com>
2018-10-09 00:37:39 -0300

    Alterando o include para Require Once

--- include/exemplo-02.php
+++ include/exemplo-02.php
@@ -1,6 +1,7 @@
 <?php
 	
-	include "exemplo-01.php";
+	//include "exemplo-01.php"; CHAMA O ARQUIVO SEM FAZER VERIFICAÇÕES
+require_once "exemplo-01.php"; //CHAMA O ARQUIVO E SE TIVER QUALQUER IRREGULARIDADE DA UM ERRO FATAL
 
 	$resultado = somar(2,2);
 
